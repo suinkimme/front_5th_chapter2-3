314b3d179cf51c9af0657f0b8028f95f4997e0a0
Suin Kim <contact@suinkim.me>
2025-04-28 15:54:22 +0900

    Layout 생성 및 Outlet으로 페이지 렌더링 변경

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,15 @@
-import { BrowserRouter as Router } from "react-router-dom"
-import Header from "../widgets/ui/Header.tsx"
-import Footer from "../widgets/ui/Footer.tsx"
-import PostsManagerPage from "../pages/PostsManagerPage.tsx"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Layout } from "@/widgets/Layout/ui"
+import PostsManagerPage from "@/pages/PostsManagerPage"
 
 const App = () => {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
-        </main>
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<PostsManagerPage />} />
+        </Route>
+      </Routes>
     </Router>
   )
 }
