eaf8f2f1c73c7ae8a6fceba4474cf4a310352111
Suin Kim <contact@suinkim.me>
2025-04-28 15:57:34 +0900

    widgets 폴더명 소문자로 변경

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { Layout } from "@/widgets/Layout/ui"
+import { Layout } from "@/widgets/layout/ui"
 import PostsManagerPage from "@/pages/PostsManagerPage"
 
 const App = () => {
--- src/widgets/Layout/ui/Layout.tsx
+++ src/widgets/Layout/ui/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
-import { Header } from "@/widgets/Header/ui"
-import { Footer } from "@/widgets/Footer/ui"
+import { Header } from "@/widgets/header/ui"
+import { Footer } from "@/widgets/footer/ui"
 
 const Layout = () => {
   return (
